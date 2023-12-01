fbaeb91ceeb2b7f62ac014b73731906b4f953108
Tam <mthanimr24@gmail.com>
2023-12-01 00:50:13 +0000

    basic login page with fancy button

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,11 @@
 module.exports = function(app, siteData){
     //handle routes here
 
-    app.get("/", function(req,res){
+    app.get("/", function(req, res){
+        res.render("login.ejs", siteData);
+    });
+
+    app.get("/home", function(req,res){
         res.render("index.ejs", siteData);
     });
 
