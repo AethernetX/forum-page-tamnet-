f9f1488645dac252febff1d9e3e7dc6bb971ab95
Tam <mthanimr24@gmail.com>
2023-12-10 18:24:51 +0000

    new users can be added

--- routes/main.js
+++ routes/main.js
@@ -20,8 +20,7 @@ module.exports = function(app, siteData){
 
     app.post("/register", function(req, res){
         let sqlquery = "SELECT * FROM users WHERE username= ?"
-
-        // execute sql query
+        //check if username is taken
         db.query(sqlquery, [req.body.username], (err, result) => {
             //err check
             if(err){
@@ -34,13 +33,21 @@ module.exports = function(app, siteData){
             if(result.length > 0){
                 newData.errMessage = "Username is taken";
                 res.render("register.ejs", newData);
-            } else if(req.body.username.length < 3){
-                newData.errMessage = "Username is too short";
+            } else if(req.body.username.length < 3 || req.body.username.length > 15){
+                newData.errMessage = "Username must have 3 - 15 characters";
                 res.render("register.ejs", newData);               
             }
-            
+            //if nothing fails insert a new user
             else {
-                res.redirect("/registered");
+                let insertQuery = "INSERT INTO users (username, password) VALUES (?,?)";
+                db.query(insertQuery, [req.body.username, req.body.password], (err) => {
+                    //err check
+                    if(err){
+                        res.redirect("./");
+                    } else {       
+                        res.redirect("/registered");
+                    }
+                });
             }
         });
     });
