eb638057775e19a9a9812c63b82f8ef5f2433e7b
Tam <mthanimr24@gmail.com>
2023-12-21 02:09:35 +0000

    tweaked db

--- create_db.sql
+++ create_db.sql
@@ -1,76 +1,48 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
-
 -- -----------------------------------------------------
--- Schema tamnet
+-- Make database tamnet
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `tamnet` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `tamnet` ;
+CREATE DATABASE tamnet;
+USE tamnet ;
 
 -- -----------------------------------------------------
 -- Table `tamnet`.`posts`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tamnet`.`posts` (
-  `post_id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(15) NULL DEFAULT NULL,
-  `description` VARCHAR(400) NULL DEFAULT NULL,
-  `user_id` INT NULL DEFAULT NULL,
-  `topic_id` INT NULL DEFAULT NULL,
-  `post_time` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`post_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 11
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE posts (
+  post_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(15),
+  description VARCHAR(400),
+  user_id INT,
+  topic_id INT,
+  post_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (post_id));
 
 
 -- -----------------------------------------------------
 -- Table `tamnet`.`topic_user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tamnet`.`topic_user` (
-  `user_id` INT NOT NULL,
-  `topic_id` INT NULL)
-ENGINE = InnoDB
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE topic_user (
+  user_id INT NOT NULL,
+  topic_id INT NULL);
 
 
 -- -----------------------------------------------------
 -- Table `tamnet`.`topics`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tamnet`.`topics` (
-  `topic_id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(21) NULL DEFAULT NULL,
-  `description` VARCHAR(200) NULL DEFAULT NULL,
-  PRIMARY KEY (`topic_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE topics (
+  topic_id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(21),
+  description VARCHAR(200),
+  PRIMARY KEY (topic_id));
 
 
 -- -----------------------------------------------------
 -- Table `tamnet`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `tamnet`.`users` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(15) NULL DEFAULT NULL,
-  `password` VARCHAR(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`user_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE users (
+  user_id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(15),
+  password VARCHAR(20),
+  PRIMARY KEY (user_id));
 
 CREATE USER 'tamAdmin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'tam2424';
 GRANT ALL PRIVILEGES ON tamnet.* TO 'tamAdmin'@'localhost';
